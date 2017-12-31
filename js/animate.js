0e599aa9212ae73851efbd637dc9f8d65042328d
shallweis <935244274@qq.com>
2017-12-31 20:41:10 +0800

    'xxx'

--- js/animate.js
+++ js/animate.js
@@ -1,12 +1,17 @@
+ 
+
+
+
+
 export const config =  [
-    [{
+    [{//1
         name: 'title',
         cls: 'animated fadeInLeft'
     }, {
         name: 'play-btn' ,
         cls: 'animated infinite pulse'    
     }], 
-    [{
+    [{//2
         name: 'f1',
         cls: 'animated fadeIn delay2'
     },
@@ -29,7 +34,7 @@ export const config =  [
         name: 'b1',
         cls: 'animated infinite pulse'
     }
-    ], [
+    ], [//3
         {
             name: 'title',
             cls: 'animated fadeInRight'
@@ -54,5 +59,279 @@ export const config =  [
             name: 'a-btn-3',
             cls: 'animated fadeInLeft delay5'
         },
+    ], 
+    [{ //4
+        name: 'a1',
+        cls: 'animated fadeInLeft'
+    },
+    {
+        name: 'a2',
+        cls: 'animated fadeInLeft delay1'
+    },
+    {
+        name: 'a3',
+        cls: 'animated fadeInRight delay2'
+    },
+    {
+        name: 'a4',
+        cls: 'animated fadeInRight delay3'
+    },
+    {
+        name: 'a5',
+        cls: 'animated fadeInRight delay4'
+    },
+    {
+        name: 'a6',
+        cls: 'animated fadeInRight delay5'
+    },
+    {
+        name: 'a7',
+        cls: 'animated fadeInRight delay6'
+    },
+    {
+        name: 'a8',
+        cls: 'animated fadeInRight delay9'
+    },
+    {
+        name: 'a9',
+        cls: 'animated fadeInRight delay10'
+    },
+    {
+        name: 'a10',
+        cls: 'animated fadeInRight delay7'
+    }],
+    [{ //5
+        name: 'title',
+        cls: 'animated fadeInLeft'
+    },
+    {
+        name: 'rotate-item',
+        cls: 'rotate'
+    },
+    {
+        name: 'rotate-box',
+        cls: 'animated fadeIn delay1'
+    },
+    {
+        name: 'text-1',
+        cls: 'animated fadeInLeft delay3'
+    },
+    {
+        name: 'text-2',
+        cls: 'animated fadeInRight delay5'
+    },
+    {
+        name: 'text-3',
+        cls: 'animated fadeInLeft delay4'
+    },
+    {
+        name: 'text-4',
+        cls: 'animated fadeInRight delay6'
+    }],
+    [{ //6
+        name: 'bg1',
+        cls: 'animated fadeInLeft'
+    },
+    {
+        name: 'bg2',
+        cls: 'animated fadeInLeft delay2'
+    },
+    {
+        name: 'bg3',
+        cls: 'animated fadeInLeft delay1'
+    },
+    {
+        name: 'bg4',
+        cls: 'animated fadeInLeft delay3'
+    }
+    ],[{ //7
+        name: 'a-btn-1',
+        cls: 'animated fadeInLeft delay'
+    },
+    {
+        name: 'a-btn-2',
+        cls: 'animated fadeInLeft delay1'
+    },
+    {
+        name: 'a-btn-3',
+        cls: 'animated fadeInLeft delay2'
+    },
+    {
+        name: 'a-btn-4',
+        cls: 'animated fadeInLeft delay3'
+    },
+    {
+        name: 'zfb1',
+        cls: 'animated   pulse infinite'
+    },
+    {
+        name: 'zfb2',
+        cls: 'animated   pulse infinite'
+    },
+    {
+        name: 'zfb3',
+        cls: 'animated   pulse infinite'
+    },
+    {
+        name: 'zfb4',
+        cls: 'animated   pulse infinite'
+    },
+    {
+        name: 'zfb5',
+        cls: 'animated   pulse infinite'
+    }],
+    [{ //8
+        name: 'page-title',
+        cls: 'animated fadeInLeft'
+    },
+    {
+        name: 'title',
+        cls: 'animated fadeInLeft delay1'
+    },
+    {
+        name: 'text-1',
+        cls: 'animated fadeInRight delay2'
+    },
+    {
+        name: 'text-2',
+        cls: 'animated fadeInRight delay3'
+    },
+    {
+        name: 'text-3',
+        cls: 'animated fadeInRight delay4'
+    },
+    {
+        name: 'text-4',
+        cls: 'animated fadeInRight delay5'
+    },
+    {
+        name: 'text-5',
+        cls: 'animated fadeInRight delay6'
+    },
+    {
+        name: 'text-6',
+        cls: 'animated fadeInRight delay7'
+    }],[
+        //9
+        { 
+            name: 'page-title',
+            cls: 'animated fadeInLeft'
+        },
+        {
+            name: 'title',
+            cls: 'animated fadeInLeft delay1'
+        },
+        {
+            name: 'left',
+            cls: 'animated fadeIn delay2'
+        },
+        {
+            name: 'right',
+            cls: 'animated fadeIn delay3'
+        },
+        {
+            name: 'right-1',
+            cls: 'animated fadeInRight delay4'
+        },
+        {
+            name: 'right-2',
+            cls: 'animated fadeInRight delay5'
+        },
+        {
+            name: 'right-3',
+            cls: 'animated fadeInRight delay6'
+        }
+    ],[
+        { 
+            name: 'page-title',
+            cls: 'animated fadeInLeft'
+        },
+        {
+            name: 'title',
+            cls: 'animated fadeInLeft delay1'
+        }, //10
+        {
+            name: 'tu1',
+            cls: 'animated fadeInLeft delay2'
+        }, 
+        {
+            name: 'tu2',
+            cls: 'animated fadeInLeft delay3'
+        }, 
+        {
+            name: 'tu3',
+            cls: 'animated fadeInLeft delay4'
+        }, 
+        {
+            name: 'tu4',
+            cls: 'animated fadeInLeft delay5'
+        }, 
+        {
+            name: 'title2',
+            cls: 'animated fadeIn delay6'
+        },
+        {
+            name: 'zu-1',
+            cls: 'animated fadeIn delay7'
+        },
+        {
+            name: 'zu-2',
+            cls: 'animated fadeIn delay8'
+        },
+        {
+            name: 'zu-3',
+            cls: 'animated fadeIn delay9'
+        },
+        {
+            name: 'zu-4',
+            cls: 'animated fadeIn delay10'
+        }
+    ],[
+        //11
+        {
+        name: 'txt-11',
+        cls: 'animated fadeInLeft delay'
+    },
+    {
+        name: 'txt11-1',
+        cls: 'animated fadeInLeft delay2'
+    },
+    {
+        name: 'txt11-2',
+        cls: 'animated fadeInLeft delay3'
+    },
+    {
+        name: 'txt11-3',
+        cls: 'animated fadeInLeft delay4'
+    }],[
+        //12
+    ]
+    ,[
+        //13
+        {
+            name: 'line',
+            cls: 'animated fadeInDown delay1'
+        },
+        {
+            name: 'text',
+            cls: 'animated fadeInRight delay1'
+        },
+        {
+            name: 'a-btn-1',
+            cls: 'animated fadeInLeft delay2'
+        },
+        {
+            name: 'a-btn-2',
+            cls: 'animated fadeInLeft delay3'
+        },
+        {
+            name: 'a-btn-3',
+            cls: 'animated fadeInLeft delay4'
+        }
+    ]
+    ,[
+        //14
+       
     ]
 ] 
+
